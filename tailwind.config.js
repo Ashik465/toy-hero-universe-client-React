43c91f004bd29a02931666cf6a2ff4c1c6977844
Ashik465 <helloashikul@gmail.com>
2023-05-18 03:04:06 +0600

    file-structure-done

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#FFFF",
+        
+"secondary": "#6202e8",
+        
+"accent": "#2c0977",
+        
+"neutral": "#241924",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#66C9EA",
+        
+"success": "#13966C",
+        
+"warning": "#F6AE31",
+        
+"error": "#F7765F",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
