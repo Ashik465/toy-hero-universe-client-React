e96e604aec75b39d19b6cea04f546e6da8bd69ed
Ashik465 <helloashikul@gmail.com>
2023-05-19 00:42:55 +0600

    gallery-section-done

---
+++ src/pages/Home/gallary/Gallary.jsx
@@ -0,0 +1,54 @@
+import gallery1 from '../../../assets/gallery1.jpg'
+import gallery2 from '../../../assets/gallery2.jpg'
+import gallery3 from '../../../assets/gallery3.jpg'
+import gallery4 from '../../../assets/gallery4.jpg'
+import gallery5 from '../../../assets/gallery5.jpg'
+import gallery6 from '../../../assets/gallery6.jpg'
+import gallery7 from '../../../assets/gallery7.jpg'
+
+const Gallary = () => {
+    return (
+        <>
+
+<div className="container mx-auto py-8">
+     <div className="text-center">
+     <h2 className="text-4xl text-[#FFC107]  font-bold my-10">Gallery Section</h2>
+     </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 md:gap-8">
+        {/* Add your gallery items here */}
+       
+       <div className='md:row-span-2 md:col-span-2'>
+         <img src={gallery1} alt="" className="w-full h-full object-cover rounded-xl  " /> 
+         </div>
+       <div>
+         <img src={gallery7} alt="" className="w-full h-full object-cover rounded-xl" /> 
+         </div>
+       <div>
+         <img src={gallery2} alt="" className="w-full h-full object-cover rounded-xl" /> 
+         </div>
+       <div className='hidden md:flex'>
+         <img src={gallery3} alt="" className="w-full h-full object-cover rounded-xl" /> 
+         </div>
+       <div>
+         <img src={gallery4} alt="" className="w-full h-full object-cover rounded-xl" /> 
+         </div>
+       <div className='md:hidden'>
+         <img src={gallery5} alt="" className="w-full h-full object-cover rounded-xl" /> 
+         </div>
+       <div className='md:hidden'>
+         <img src={gallery6} alt="" className="w-full h-full object-cover rounded-xl" /> 
+         </div>
+
+               
+
+
+      </div>
+    </div>
+
+
+
+        </>
+    );
+};
+
+export default Gallary;
