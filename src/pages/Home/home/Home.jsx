1bb00524dc0b216a387d795f9a0ce66ddf7728d6
Ashik <helloashikul4@gmail.com>
2023-05-19 09:21:13 +0600

    privateroute and loader done

--- src/pages/Home/home/Home.jsx
+++ src/pages/Home/home/Home.jsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
 import Banner from "../banner/Banner";
 import Brand from "../brand/Brand";
 import Gallary from "../gallary/Gallary";
 import OurServices from "../ourServices/OurServices";
+import LoadingSpinner from "../../../loader/LoadingSpinner";
 
 
 const Home = () => {
+
+    const {loader } = useContext(AuthContext)
+
+// loader 
+
+if(loader){
+  return <LoadingSpinner></LoadingSpinner>
+}
     return (
         <>
             <Banner></Banner>
