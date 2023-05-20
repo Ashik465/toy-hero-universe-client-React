5e1365ec1d0e487156d9d8a601a1155ecaad91d1
Ashik <helloashikul4@gmail.com>
2023-05-20 13:39:29 +0600

    404 page done

---
+++ src/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,18 @@
+
+
+import error from '../../assets/error.svg'
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <>
+           <div className=' text-center'>
+            <div className='text-7xl font-bold'>Back to home-page <br /> <Link className='btn btn-main mb-3' to='/'>Home-page</Link>    </div>
+<div className='  flex justify-center'><img src={error} alt="" /></div>
+            
+        </div> 
+        </>
+    );
+};
+
+export default ErrorPage;
